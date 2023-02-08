9505f0ce9a670ddf5de420d5ffad9c75a2bdf15d
Jessica <jessicatong43@gmail.com>
2023-02-08 12:14:31 -0800

    First commit. Passing tests for Functional pattern's stack.js. Moving onto Functional pattern's queue.js next.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,23 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var keys = 0;
+
   // Implement the methods below
   someInstance.push = function(value) {
+    keys++;
+    someInstance[keys] = value;
   };
 
   someInstance.pop = function() {
+    var popped = someInstance[keys];
+    delete popped;
+    keys > 0 ? keys-- : keys = 0;
+    return popped;
   };
 
   someInstance.size = function() {
+    return keys;
   };
 
   return someInstance;
